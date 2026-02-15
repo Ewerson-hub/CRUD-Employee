a197c1fe00d7daa47ed5c0259d61a3ed2bac3996
Ewerson-hub <ewersonl10@gmail.com>
2026-02-15 12:14:56 -0300

    feat: adding the creation of the database and table if it does not already exists

--- scripts/database/conection.js
+++ scripts/database/conection.js
@@ -1,16 +1,42 @@
 
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 require('dotenv').config();
+
+const DB_NAME = 'company'
+
+async function createDatabase() {
+    const tempConnection = await mysql.createConnection({
+        host:process.env.DB_LOGIN,
+        user:process.env.DB_USER,
+        password: process.env.DB_PASS,
+    })
+
+    await tempConnection.query(`CREATE DATABASE IF NOT EXISTS ${DB_NAME}`);
+    await tempConnection.query(`USE ${DB_NAME}`);
+    await tempConnection.query(`
+        CREATE TABLE IF NOT EXISTS employee (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            name VARCHAR(255) NOT NULL,
+            sector VARCHAR(255) NOT NULL
+        );  
+    `)
+    await tempConnection.end();
+}
+
+createDatabase();
 
 const pool = mysql.createPool({
     host : process.env.DB_LOGIN,
-    database : 'My_company',
-    user : 'root', 
+    database : DB_NAME,
+    user : process.env.DB_USER, 
     password: process.env.DB_PASS,
 
     waitForConnections:true,
     connectionLimit: 10
 })
+
+
+
 
 module.exports = pool;
 
