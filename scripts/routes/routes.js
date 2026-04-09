71b8c19e8bd94e13aa3595bc03418c34ee341f14
Ewerson-hub <ewersonl10@gmail.com>
2026-04-09 11:04:28 -0300

    feat: adding a user operations routes

--- scripts/routes/routes.js
+++ scripts/routes/routes.js
@@ -1,13 +1,46 @@
 const express = require('express');
 const router = express.Router();
-const {selectAll, insertNewEmployee,selectById, updateEmployee, deleteEmployee} = require('../controls/controls');
+const {selectAll, insertNewEmployee,selectById, updateEmployee, deleteEmployee} = require('../controls/employeeOperations');
+const {createUser, verifyUserAcess} = require('../controls/usersOperations');
 
-
+router.get('/signUpScreen', (req, res) => {
+    res.render('signUpScreen');
+})
 router.get('/', async (req, res) => {
+    res.render('loginScreen');
+})
+
+router.post('/createUser', async (req, res) => {
+    try{
+
+        const userCreated = await createUser(req.body.login, req.body.password)
+
+        if (userCreated){ 
+            console.log('User Created !') 
+            res.redirect('/'); 
+        }
+
+    } catch (error){
+        console.log('Error on create user => ', error);
+    }
+    
+})
+
+router.post('/signInUser', async (req, res) => {
+    let userVerification = await verifyUserAcess(req.body.login, req.body.password)
+
+    if(userVerification){
+        //criar os tokens e a sessao
+        res.redirect('/showAll')
+    }else{
+        res.render('errorPage')
+    }
+})
+
+router.get('/showAll', async (req, res) => {
     const data = await selectAll();
     res.render('home', {'employee' : data});
 })
-
 router.get('/registerEmployee', (req, res) => {
     res.render('registerEmployee')
 })
@@ -17,7 +50,7 @@ router.post('/registerEmployee',  async (req, res) => {
 
    (sucessTransation)? console.log('Employee registered') : console.log('Employee NOT registered'); 
 
-   res.redirect('/');
+   res.redirect('/showAll');
 })
 
 router.get('/updateEmployee/:id', async (req, res) => {
@@ -31,7 +64,7 @@ router.post('/updateEmployee', async (req, res) => {
 
     (sucessTransation)? console.log('Employee Updated') : console.log('Employee NOT Updated'); 
 
-    res.redirect('/');
+    res.redirect('/showAll');
 })
 router.get('/deleteEmployeePage/:id', async (req,res) => {
     const data = await selectById(req.params.id);
@@ -42,7 +75,7 @@ router.post('/delete', async (req, res) => {
 
     (sucessTransation)? console.log('Employee Deleted') : console.log('Employee NOT Deleted'); 
 
-    res.redirect('/');
+    res.redirect('/showAll');
 })
 
 module.exports = router
