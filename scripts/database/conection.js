cbbf49701c5bcdce0dcb9478ff0eb3d669fc11c9
Ewerson-hub <ewersonl10@gmail.com>
2026-04-09 10:53:39 -0300

    refactor: adding a table scheema for  employee and users

--- scripts/database/conection.js
+++ scripts/database/conection.js
@@ -20,6 +20,16 @@ async function createDatabase() {
             sector VARCHAR(255) NOT NULL
         );  
     `)
+    await tempConnection.query(`
+        CREATE TABLE IF NOT EXISTS users (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                login VARCHAR(255) NOT NULL,
+                password VARCHAR(255) NOT NULL,
+                is_admin TINYINT DEFAULT 0 
+        );   
+    `)
+    
+
     await tempConnection.end();
 }
 
